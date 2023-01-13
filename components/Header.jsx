bf7a0a24f0b86cde057428a9c9724f65b21e31e0
bbtit <the8butterfly8dream@gmail.com>
2023-01-13 12:27:16 +0900

    ✨ Header.jsxのリンクを編集・不要な部分のコメントアウト

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,16 +9,16 @@ export default function Header() {
           <span className="ml-3 text-xl">Gradable</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">Kibaco</a>
-          <a className="mr-5 hover:text-gray-900">CampusSquare</a>
-          <a className="mr-5 hover:text-gray-900">Syllabus</a>
-          <a className="mr-5 hover:text-gray-900">About</a>
+          <a href="https://kibaco.tmu.ac.jp/portal" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-900">Kibaco</a>
+          <a href="https://jjh.tmu.ac.jp/campusweb/campusportal.do" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-900">CampusSquare</a>
+          <a href="http://www.kyouikujouhou.eas.tmu.ac.jp/syllabus/flame.html" target="_blank" rel="noopener noreferrer" className="mr-5 hover:text-gray-900">Syllabus</a>
+          {/* <a className="mr-5 hover:text-gray-900">About</a> */}
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+        {/* <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
           <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
           </svg>
-        </button>
+        </button> */}
       </div>
     </header>
   );
