7700a06c8e1f8a18f351500d3d2216e6a728fa2f
bbtit <the8butterfly8dream@gmail.com>
2023-01-24 12:36:03 +0900

    💄 focus時の色を変更

--- components/InputForm.jsx
+++ components/InputForm.jsx
@@ -26,13 +26,13 @@ export default function InputForm(props) {
       <div className="flex items-center justify-between mt-1">
 
         <div className="relative rounded-md shadow-sm">
-          <input type="text" value={props.text} onChange={props.handleChange} ref={inputElement} name="code" id="code" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="A0000"/>
+          <input type="text" value={props.text} onChange={props.handleChange} ref={inputElement} name="code" id="code" className="block w-full rounded-md border-gray-300 pl-7 pr-12 focus:border-blue-400 focus:ring-blue-400 sm:text-sm" placeholder="T0000"/>
 
           <div className="absolute inset-y-0 right-0 flex items-center">
             <label htmlFor="select-year" className="sr-only">
               select-year
             </label>
-            <select onChange={e => props.setYear(e.target.value)} defaultValue="2022" id="select-year" name="select-year" className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+            <select onChange={e => props.setYear(e.target.value)} defaultValue="2022" id="select-year" name="select-year" className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:border-blue-400 focus:ring-blue-400 sm:text-sm">
               <option value="2022">2022年度</option>
               <option value="2021">2021年度</option>
             </select>
